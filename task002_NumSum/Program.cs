46d87387f3b001c2470dd02cbc597d727e720360
ReZero <ghostofpast@icloud.com>
2023-04-10 14:24:32 +0300

    complete tasks

---
+++ task002_NumSum/Program.cs
@@ -0,0 +1,22 @@
+﻿//Программа, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int NumSum(int number)
+{
+    int counter = Convert.ToString(number).Length;
+    int first = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+      first = number - number % 10;
+      result = result + (number - first);
+      number = number / 10;
+    }
+    return result;
+}
+
+int final = NumSum(number);
+Console.WriteLine("Сумма цифр в числе: " + final);
---
+++ task003_Array2/Program.cs
@@ -0,0 +1,28 @@
+﻿//Программ, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 1000);
+        index++;
+    }
+}
+
+void PrintArray(int[] shelf)
+{
+    int closet = shelf.Length;
+    int position = 0;
+    while (position < closet)
+    {
+        Console.Write(shelf[position] + ", ");
+        position++;
+    }
+}
+
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
